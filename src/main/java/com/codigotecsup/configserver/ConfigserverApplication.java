2aa45a9467ca85b0d5e1b6c06913ff553661b369
neosapiens8 <neosapiens8@gmail.com>
2024-06-27 22:52:14 -0500

    actualiza configserver ok 20240627 2252

--- src/main/java/com/codigotecsup/configserver/ConfigserverApplication.java
+++ src/main/java/com/codigotecsup/configserver/ConfigserverApplication.java
@@ -14,9 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 
 public class ConfigserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
-
 }
